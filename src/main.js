68f7400b61fbc82dff8d7aff91652c724990d938
liyu <1779371276@qq.com>
2021-09-08 10:31:15 +0800

    add-content

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import ElementUI from 'element-ui' 
+/*jshint esversion: 6 */
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import ElementUI from 'element-ui'; 
 import ElementCss from 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
-Vue.use(ElementUI)
-Vue.use(ElementCss)
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
+Vue.use(ElementCss);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
